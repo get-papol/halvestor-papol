7d7ca00d28bf9464d10ca7e7ac8d9020e8c8cac5
lukman_as <lukmaantck@gmail.com>
2025-11-22 08:25:32 -0500

    fix: initial fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,50 +1,15 @@
-import * as React from 'react';
-import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
-import { setup } from 'twind';
-import { asyncVirtualSheet, getStyleTagProperties } from 'twind/server';
-import twindConfig from '../twind.config';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-const sheet = asyncVirtualSheet();
-
-setup({ ...twindConfig, sheet });
-
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    sheet.reset();
-    const initialProps = await Document.getInitialProps(ctx);
-    const { id, textContent } = getStyleTagProperties(sheet);
-    const styleProps = {
-      id,
-      key: id,
-      dangerouslySetInnerHTML: {
-        __html: textContent,
-      },
-    };
-
-    return {
-      ...initialProps,
-      styles: [
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        ...initialProps.styles,
-        React.createElement(`style`, styleProps),
-      ],
-    };
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <script async src="https://scripts.simpleanalyticscdn.com/latest.js" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script async src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
