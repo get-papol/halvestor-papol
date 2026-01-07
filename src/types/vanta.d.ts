e1fcd81067757c40efceb4b21d052a238037d6ec
lukman_as <lukmaantck@gmail.com>
2026-01-07 15:46:16 -0500

    fix: bug fix

---
+++ src/types/vanta.d.ts
@@ -0,0 +1,9 @@
+declare module 'vanta/dist/vanta.waves.min' {
+  const WAVES: any;
+  export default WAVES;
+}
+
+declare module 'vanta/dist/vanta.topology.min' {
+  const TOPOLOGY: any;
+  export default TOPOLOGY;
+}
